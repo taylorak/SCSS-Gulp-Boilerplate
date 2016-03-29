2c03fff387e8ed33d1916aa335f66482527775fe
Taylor Kennedy <taykenned@gmail.com>
2016-03-28 15:44:38 -1000

    adds browser-sync to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,24 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+  browserSync.init({
+      server: "./public"
+  });
+
+  gulp.watch("scss/**/*.scss", ['sass']);
+  gulp.watch("public/*.html").on('change', browserSync.reload);
+});
 
 gulp.task('sass', function () {
   return gulp.src('./scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./public/css'));
+    .pipe(gulp.dest('./public/css'))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
-});
+gulp.task('default', ['serve'])
